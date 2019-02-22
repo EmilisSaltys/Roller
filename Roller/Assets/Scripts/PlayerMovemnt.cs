8f3ba85b1f50eaf9018a2800204ec07448bd0951
EmilisSaltys <47056599+EmilisSaltys@users.noreply.github.com>
2019-02-22 18:42:46 +0200

    FirstDay

---
+++ Roller/Assets/Scripts/PlayerMovemnt.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovemnt : MonoBehaviour
+{
+
+    public Rigidbody rb;
+  
+    void Start()
+    {
+
+        rb.GetComponent<Rigidbody>();
+
+    }
+
+    
+    void Update()
+    {
+
+        rb.velocity = new Vector3(0, 0, 13);
+
+        if (Input.GetButtonDown("D"))
+        {
+            rb.velocity = new Vector3(7, 0, 0);
+        }
+
+
+
+    }
+}
